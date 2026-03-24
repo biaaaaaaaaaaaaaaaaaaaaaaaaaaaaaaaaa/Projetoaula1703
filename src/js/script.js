4e7e99b84cfe08362e2cd7a9f6eaf3b96068d41b
biaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa <biassalve@gmail.com>
2026-03-24 11:44:48 -0300

    feat:Declaraçoes e variaveis js

---
+++ src/js/script.js
@@ -0,0 +1,45 @@
+// Declraçoes e Variaveis
+
+// Var, Let e Const
+//var x=10;
+//console.log(x);
+//var x=20;
+//console.log(x)
+// var; pode redeclarar e acessar fora do escopo
+//let nao pode redeclarar e nao consegue acessar fora do escopo
+
+
+var exemplo1="Ola dev var";
+console.log(exemplo1);
+
+let exemplo2="Ola dev let";
+console.log(exemplo2);
+
+const exemplo3="Ola dev const";
+console.log(typeof exemplo3);
+
+let exemplo4=20;
+console.log(typeof exemplo4);
+
+let exemplo5=true;
+console.log( typeof exemplo5);
+
+let exempleo6={nome:"fiap"};
+console.log(typeof exempleo6);
+
+let emexplo7=["Java", "python", "C#"];
+console.log(emexplo7);
+
+//variável indefinida
+let exemplo8;
+console.log(exemplo8);
+
+// variável vazia
+let exemplo9=null;
+console.log(exemplo9)
+
+//( método)
+//{objeto}
+//[array]
+
+ 
